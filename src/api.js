87b812e801a0f24f3d2c0b403934f55999f8eefc
Sean Dinwiddie <seandinwiddie@gmail.com>
2024-10-21 19:56:47 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors());
